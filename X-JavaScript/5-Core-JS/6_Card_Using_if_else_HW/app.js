6df2d155579181b58be5a990027d5ff1707f7bda
Vibhav Kumar Dubey <110058521+vibhavdubey@users.noreply.github.com>
2023-10-05 20:36:58 +0530

    update

--- X-JavaScript/5-Core-JS/6_Card_Using_if_else_HW/app.js
+++ X-JavaScript/5-Core-JS/6_Card_Using_if_else_HW/app.js
@@ -2,16 +2,14 @@ const user=["vibhav","rohit","sourav","akash","ankit","rahul","andy","vibhav","r
 let userEmail=["vibhav@gmail.com","rohit@gmail.com","sourav@gmail.com","akash@gmail.com","ankit@gmail.com","rahul@gmail.com","andy@gmail.com","vibhav@gmail.com","rohit@gmail.com","sourav@gmail.com","akash@gmail.com","ankit@gmail.com"];
 let post=["img/image0.jpg","img/image1.jpg","img/image2.jpg","img/image3.jpg","img/image4.jpg","img/image5.jpg","img/image6.jpg","img/image0.jpg","img/image1.jpg","img/image2.jpg","img/image3.jpg","img/image4.jpg"]
 
-let count=6;
+let count=5;
 
         if(count==1){
-            for (let i = 0; i < count; i++) {
                 document.write(`<div class="box bg${count}">
-                    <h1>User : ${user[i]}</h1>
-                    <h2>Email : ${userEmail[i]}</h2>
-                    <img src="${post[i]}" alt="image">
+                    <h1>User : ${user[count]}</h1>
+                    <h2>Email : ${userEmail[count]}</h2>
+                    <img src="${post[count]}" alt="image">
                 </div>`);
-            }
         }else if(count==2){
             for (let i = 0; i < count ; i++) {
                 document.write(`<div class="box bg${i}">
