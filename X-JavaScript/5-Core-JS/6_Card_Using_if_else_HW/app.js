e1049040e3649c1462a5831446104bbc3ce6c727
Vibhav Kumar Dubey <110058521+vibhavdubey@users.noreply.github.com>
2023-10-07 22:12:50 +0530

    basic added

--- X-JavaScript/5-Core-JS/6_Card_Using_if_else_HW/app.js
+++ X-JavaScript/5-Core-JS/6_Card_Using_if_else_HW/app.js
@@ -2,7 +2,7 @@ const user=["vibhav","rohit","sourav","akash","ankit","rahul","andy","vibhav","r
 let userEmail=["vibhav@gmail.com","rohit@gmail.com","sourav@gmail.com","akash@gmail.com","ankit@gmail.com","rahul@gmail.com","andy@gmail.com","vibhav@gmail.com","rohit@gmail.com","sourav@gmail.com","akash@gmail.com","ankit@gmail.com"];
 let post=["img/image0.jpg","img/image1.jpg","img/image2.jpg","img/image3.jpg","img/image4.jpg","img/image5.jpg","img/image6.jpg","img/image0.jpg","img/image1.jpg","img/image2.jpg","img/image3.jpg","img/image4.jpg"]
 
-let count=5;
+let count=12;
 
         if(count==1){
                 document.write(`<div class="box bg${count}">
