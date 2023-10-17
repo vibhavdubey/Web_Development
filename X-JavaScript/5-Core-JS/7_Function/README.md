98f4a5dc302cc863d167a22e8d55ea2c30ccf261
Vibhav Kumar Dubey <110058521+vibhavdubey@users.noreply.github.com>
2023-10-17 20:35:01 +0530

    basic added

---
+++ X-JavaScript/5-Core-JS/7_Function/README.md
@@ -0,0 +1,8 @@
+# JS Function (process) : js + ecmascript + typescript
+## Function type
+
+> pre define (log(),write())
+> user define
+- define the function (R)
+> call the function (S)
+> passing the argument (D)
---
+++ X-JavaScript/5-Core-JS/7_Function/app.js
@@ -0,0 +1,29 @@
+// define the variable
+// let welcome="Welcome to variable data";
+// call the variable
+//console.log(welcome);
+hello();
+
+//define the function
+function hello() {
+    document.write("Welcome to function");
+    console.log("Welcome to function");
+}
+
+// call the function
+hello();
+hello();
+
+//example 
+function ducat(x){
+    console.log(x);
+}
+
+// example
+function emp(data){
+    document.write(`<h1>Welcome ${data}</h1>`);
+}
+//call
+emp("vibhav");
+emp("Ducat India");
+
